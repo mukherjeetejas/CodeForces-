3da48f48084d2cbbe208f7e5ddcf4e856a7d99e2
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2020-10-10 01:08:13 +0530

    submitted a solution

---
+++ 75A Life without zeroes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int nozeroes(int n)
+{
+    int result=0, count, curr;
+    count = 0;
+    while(n>0)
+    {
+        curr=n%10;
+        if(curr!=0)
+        {
+            result += curr * pow(10,count);
+            count++;
+        }
+        n=n/10;
+    }
+    if (result==0)
+    return n;
+    else 
+    return result;
+}
+int main()
+{
+    int num1,num2,res;
+    cin>>num1>>num2;
+    res=num1+num2;
+    if(nozeroes(res)==nozeroes(num1)+nozeroes(num2))
+    cout<<"YES";
+    else
+    cout<<"NO";
+}
