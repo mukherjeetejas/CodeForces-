e7d4708ba45cc7cd416a960405afeca4d5be9a1e
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2020-10-10 12:40:06 +0530

    805B

---
+++ 805B 3-Palindrome.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string nopalindrome(int n)
+{
+    string a;
+    for(int i=0;i<n;i++)
+    {
+        a+='a';
+    }
+    for (int i=1;i<n-1;i++)
+    {   
+        if (a[i+1]==a[i-1])
+        {
+            if (a[i-1]=='a')
+                a[i+1]='b';
+            else
+                a[i+1]='a';
+        }
+    }
+    return a;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<nopalindrome(n);
+    return 0;
+}
