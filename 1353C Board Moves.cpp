56f60ec20c5bfaf56262ba51e39e9a436a064a20
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2020-10-14 15:36:21 +0530

    1353C

---
+++ 1353C Board Moves.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll N;
+        cin>>N;
+        ll n = N/2;
+        ll moves = 0;
+        ll o =3;
+        for (int i=1;i<=n;i++)
+        {
+            moves+=(4*o-4)*i;
+            o+=2;
+        }
+        cout<<moves<<'\n';
+    }
+}
