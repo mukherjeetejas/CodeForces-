e2726b3f0acc0ef39fc6348519d06a5699902d81
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2020-11-24 22:36:04 +0530

    1454A

---
+++ 1454A Special Permutation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(a,b) for(int i=a;i<b;i++)
+#define ll long long
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        FOR(0,n-1)
+        {
+            cout<<i+2<<" ";
+        }
+        cout<<"1"<<'\n';
+    }
+}
