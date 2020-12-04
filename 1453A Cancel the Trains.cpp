c7cfee9484cc926833b220841134e7fcf27e31c2
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2020-12-04 20:51:07 +0530

    1453A

---
+++ 1453A Cancel the Trains.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        int a[n], b[m];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0;i<m;i++)
+        {
+            cin>>b[i];
+        }
+        int c=0;
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<m;j++)
+                if(a[i]==b[j])
+                {
+                    c++; break;
+                }
+        }
+        cout<<c<<'\n';
+    }
+}
