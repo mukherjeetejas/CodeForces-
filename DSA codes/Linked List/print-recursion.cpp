a6da7c3349a32c15fce7b9affb4a3a7a23f0f513
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-07-16 23:27:16 +0530

    recursive print of ll

---
+++ DSA codes/Linked List/print-recursion.cpp
@@ -0,0 +1,37 @@
+//Author Tejas Mukherjee
+//Implementation of linked list as a structure
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    if (head == NULL)
+        return;
+    else
+    {
+        cout << head->data << '\n';
+        printlist(head->next);
+    }
+}
+
+int main()
+{
+    Node *head = new Node(2);
+    head->next = new Node(0);
+    head->next->next = new Node(5);
+    head->next->next->next = new Node(5);
+
+    printlist(head);
+}
