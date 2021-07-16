c196a07ea648e9484fd8455adfc2666c899dfa59
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-07-16 23:17:02 +0530

    bug fixes

--- DSA codes/Linked List/implementation-struct.cpp
+++ DSA codes/Linked List/implementation-struct.cpp
@@ -12,11 +12,12 @@ struct Node
     }
 };
 
-void main()
+int main()
 {
-    Node head = new Node(10);
-    Node temp1 = new Node(20);
-    Node temp2 = new Node(30);
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
     head->next = temp1;
     temp1->next = temp2;
+    cout << head->data;
 }
