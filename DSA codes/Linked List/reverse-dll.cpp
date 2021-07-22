a75b0608fbc87826fc7c7983165e5fb0728032fc
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-07-22 23:21:55 +0530

    ff

---
+++ DSA codes/Linked List/reverse-dll.cpp
@@ -0,0 +1,59 @@
+//Author Tejas Mukherjee
+//Implementation of linked list as a structure
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << '\t';
+        curr = curr->next;
+    }
+    cout << '\n';
+}
+
+Node *reversedll(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        Node *temp = curr->next;
+        curr->next = curr->prev;
+        curr->prev = curr->next;
+        curr = curr->prev;
+    }
+    return curr;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+    temp1->prev = head;
+    printlist(head);
+    cout << "After reversal : ";
+    head = reversedll(head);
+    printlist(head);
+}
