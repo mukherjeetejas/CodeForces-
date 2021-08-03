21f742bc93c74dea67110e84adcb9af320844c5a
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-03 08:13:31 +0530

    delete tail dll

---
+++ DSA codes/Linked List/delete-tail-dll.cpp
@@ -0,0 +1,64 @@
+//Author Tejas Mukherjee
+//Implementation of linked list as a structure
+//delete head of doubly linked list
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << '\t';
+        curr = curr->next;
+    }
+    cout << '\n';
+}
+
+Node *deletetaildll(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->prev->next = NULL;
+    delete curr;
+    return head;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+    temp1->prev = head;
+    printlist(head);
+    cout << "After head deletion : ";
+    head = deletetaildll(head);
+    printlist(head);
+}
