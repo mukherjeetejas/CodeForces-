a319db342a3ff3b3d38ef81aa14ec5457c3f1f5d
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-03 07:44:02 +0530

    reverse dll final

--- DSA codes/Linked List/reverse-dll.cpp
+++ DSA codes/Linked List/reverse-dll.cpp
@@ -33,14 +33,15 @@ Node *reversedll(Node *head)
     if (head == NULL || head->next == NULL)
         return head;
     Node *curr = head;
+    Node *temp = NULL;
     while (curr != NULL)
     {
-        Node *temp = curr->next;
-        curr->next = curr->prev;
+        temp = curr->prev;
         curr->prev = curr->next;
+        curr->next = temp;
         curr = curr->prev;
     }
-    return curr;
+    return temp->prev;
 }
 
 int main()
