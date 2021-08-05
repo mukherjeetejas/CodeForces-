030491644e07625778768eebc57be80cdd925f37
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-05 23:24:27 +0530

    placeholder

---
+++ DSA codes/Linked List/middle-linkedlist.cpp
@@ -0,0 +1,55 @@
+//Author Tejas Mukherjee
+//Implementation of linked list as a structure
+// Print middle of linked list using 2 pointers tortoise and hare
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << '\t';
+        curr = curr->next;
+    }
+    cout << '\n';
+}
+
+int middlelinkedlist(Node *head)
+{
+    if (head == NULL)
+        return -1;
+    if (head->next == NULL)
+        return head->data;
+    Node *tortoise = head, *hare = head;
+    while (hare != NULL || hare->next == NULL)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    }
+    return tortoise->data;
+}
+
+int main()
+{
+    Node *head = new Node(2);
+    head->next = new Node(0);
+    head->next->next = new Node(87);
+    head->next->next->next = new Node(5);
+    head->next->next->next->next = new Node(23);
+
+    printlist(head);
+    cout << "middle element of linked list is : " << middlelinkedlist(head);
+}
