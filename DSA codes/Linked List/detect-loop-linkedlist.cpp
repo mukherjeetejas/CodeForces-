c2ae4e3f53e531aa62d800f6a60c35a3bcada452
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-12 20:53:23 +0530

    detect a loop 2

--- DSA codes/Linked List/detect-loop-linkedlist.cpp
+++ DSA codes/Linked List/detect-loop-linkedlist.cpp
@@ -31,6 +31,16 @@ void printlist(Node *head)
 
 bool loop(Node *head)
 {
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
+    return false;
 }
 
 int main()
