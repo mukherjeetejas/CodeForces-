df1dcdb96ec218c548560b27a85e06781f63e292
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-18 21:19:51 +0530

    size of a tree

---
+++ DSA codes/Tree/size-of-tree.cpp
@@ -0,0 +1,44 @@
+//Tree traversal inorder
+//POSTORDER => LEFT SUBTREE + RIGHT SUBTREE + ROOT
+//Tejas K Mukherjee
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        key = data;
+        left = right = NULL;
+    }
+};
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->key << '\t';
+}
+
+int height(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    return (max(height(root->left), height(root->right)) + 1);
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    postorder(root);
+    cout << "\nHeight of the tree is : " << height(root);
+}
